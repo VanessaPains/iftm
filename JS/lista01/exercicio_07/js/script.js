9a443a222f9fa127d1b11af5c48843397d00100c
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:49:02 -0300

    Update script.js

--- JS/lista01/exercicio_07/js/script.js
+++ JS/lista01/exercicio_07/js/script.js
@@ -1,1 +1,15 @@
+//aqui vou solicita os valores RGB
+var r = parseInt(prompt(`Digite o valor de R (0 a 255):`), 10);
+var g = parseInt(prompt(`Digite o valor de G (0 a 255):`), 10);
+var b = parseInt(prompt(`Digite o valor de B (0 a 255):`), 10);
 
+//aqui fazermos para que os valores fiquem no intervalo [0,255]
+if (isNaN(r) || isNaN(g) || isNaN(b) || r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+    document.write(`<p style='color:red; text-align:center;'>Valores inválidos! Digite apenas números entre 0 e 255.</p>`);
+} else {
+    //aqui vou monta a cor em formato rgb(r,g,b)
+    var cor = `rgb(${r},${g},${b})`;
+
+    //aqui eu vou mostra o texto com a cor escolhida
+    document.write(`<h1 style='color:${cor}; text-align:center;'>Fundamentos de Web Design II</h1>`);
+}
