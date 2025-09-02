8d9817342299c596bbec082ecc916c395396702b
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:47:41 -0300

    Update script.js

--- JS/lista01/exercicio_05/js/script.js
+++ JS/lista01/exercicio_05/js/script.js
@@ -1,1 +1,8 @@
+//eu solicito o nome completo do usuário
+var nome = prompt(`Digite seu nome completo:`);
 
+//aqui contamos a quantidade de caracteres (inclui espaços)
+var quantidade = nome.length;
+
+//aqui vamos escrever no corpo da página o resultado
+document.write(`O nome informado possui ${quantidade} caracteres.`);
