338a49f4fe8d66336e8de7d9b45bba232a69cc47
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:45:22 -0300

    Update script.js

--- JS/lista01/exercicio_02/js/script.js
+++ JS/lista01/exercicio_02/js/script.js
@@ -1,1 +1,5 @@
+// Exibe a mensagem inicial
+alert(`Pressione o botão "Ok" \npara visualizar o nome da disciplina`);
 
+// Após clicar em OK, escreve o conteúdo no corpo da página
+document.write("<h1>Fundamentos de Web Design II</h1>");
