5eee2281f17445b8613af7a7824bb2f055012441
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:46:58 -0300

    Update script.js

--- JS/lista01/exercicio_04/js/script.js
+++ JS/lista01/exercicio_04/js/script.js
@@ -1,1 +1,14 @@
+//eu vou solicita as notas do 1º e 2º bimestres
+var nota1 = parseFloat(prompt(`Digite a nota do 1º bimestre:`));
+var nota2 = parseFloat(prompt(`Digite a nota do 2º bimestre:`));
 
+//vou calcula a soma das notas
+var soma = nota1 + nota2;
+
+//vamos verifica se o aluno foi aprovado ou reprovado
+if (soma >= 60.0) {
+    alert(`Parabéns! Você foi APROVADO com ${soma} pontos.`);
+} else {
+    var falta = 60.0 - soma;
+    alert(`Você foi REPROVADO com ${soma} pontos.\nFaltaram ${falta.toFixed(1)} ponto(s) para ser aprovado.`);
+}
