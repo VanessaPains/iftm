4189b4f4f7a4ffbeb1a463989d07edea3760832d
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:38:35 -0300

    Update script.js

--- JS/lista01/exercicio_01/js/script.js
+++ JS/lista01/exercicio_01/js/script.js
@@ -1,1 +1,14 @@
 
+//aqui vamos solicita o primeiro nome
+var primeiroNome = prompt(`Digite seu primeiro nome:`);
+
+
+//aqui vamos solicita o sobrenome
+var sobrenome = prompt(`Digite seu sobrenome:`);
+
+
+//aqui vamos monta o nome completo com o espeço entre o primeiro nome e o sobrenome
+var nomeCompleto = `${primeiroNome} ${sobrenome}`;
+
+//aqui iremos exibir o resultado do nome e sobrenome completos
+alert(`Seu nome completo é: ${nomeCompleto}`);
