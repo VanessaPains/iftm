253d72227b3609051657f7d090ad1d3056e677b5
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:48:27 -0300

    Update script.js

--- JS/lista01/exercicio_06/js/script.js
+++ JS/lista01/exercicio_06/js/script.js
@@ -1,1 +1,5 @@
+//vamos solicita o nome completo do usuário
+var nome = prompt(`Digite seu nome completo:`);
 
+//vamos escrever o nome no corpo da página com css
+document.write(`<p style='color:red; font-size:20px; text-align:center;'>${nome}</p>`);
