1e44f3d693c76ac4e6c14514831f2939722b20f5
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:50:14 -0300

    Update script.js

--- JS/lista01/exercicio_09/js/script.js
+++ JS/lista01/exercicio_09/js/script.js
@@ -1,1 +1,13 @@
+//vamos solicita a expressão matemática ao usuário
+var expressao = prompt(`Digite uma expressão matemática, por exemplo: ((5*8)-9)`);
 
+try {
+    //aqui vamos valia a expressão usando eval
+    var resultado = eval(expressao);
+
+    //aqui vou mostra o resultado em um alerta
+    alert(`O resultado da expressão é: ${resultado}`);
+} catch (erro) {
+    //aqui mensagem de erro caso a expressão seja inválida
+    alert(`Expressão inválida! Por favor, digite uma expressão matemática correta.`);
+}
