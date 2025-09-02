f978652a8b836ebc37afab7c458376eb5c72dee4
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:34:22 -0300

    Update script.js

--- JS/lista01/js/script.js
+++ JS/lista01/js/script.js
@@ -1,1 +1,30 @@
+//======= INICIO INCLUIR CONTEUDO HTML EM PAGINA EXTERNAS ============
+// Função para incluir conteúdo HTML de arquivos externos
+function incluirHTML() {
+    const elementos = document.querySelectorAll('[data-include]');
+    elementos.forEach(async el => {
+        const arquivo = el.getAttribute('data-include');
+        try {
+            const resposta = await fetch(arquivo);
+            const conteudo = await resposta.text();
+            el.innerHTML = conteudo;
+        } catch (e) {
+            el.innerHTML = "<p>Erro ao carregar conteúdo.</p>";
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", incluirHTML);
+//======= FIM INCLUIR CONTEUDO HTML EM PAGINA EXTERNAS ============
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenuUl = document.querySelector(".nav-menu ul");
+
+    if (menuToggle && navMenuUl) {
+        menuToggle.addEventListener("click", function () {
+            navMenuUl.classList.toggle("open");
+        });
+    }
+});
