4f6316c177550b15fc01568bb9890d2a6cd21459
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:46:00 -0300

    Update script.js

--- JS/lista01/exercicio_03/js/script.js
+++ JS/lista01/exercicio_03/js/script.js
@@ -1,1 +1,13 @@
+//vai solicita o nome completo
+var nome = prompt(`Digite seu nome completo:`);
 
+//vamos solicita a idade
+var idade = parseInt(prompt(`Digite sua idade:`), 10);
+
+//vou so Verifica se é maior ou igual a 18 anos
+if (idade >= 18) {
+    alert(nome + `, você já POSSUI idade para tirar carteira de motorista.`);
+} else {
+    var falta = 18 - idade;
+    alert(nome + `, você ainda NÃO POSSUI idade para tirar carteira, ainda falta(m) ${falta} ano(s).`);
+}
