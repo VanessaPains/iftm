a7b8826abd2f0920588cf0226ac074526fe87c0d
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-02 17:49:37 -0300

    Update script.js

--- JS/lista01/exercicio_08/js/script.js
+++ JS/lista01/exercicio_08/js/script.js
@@ -1,1 +1,13 @@
+//aqui solicita o nome completo do usuário
+var nome = prompt(`Digite seu nome completo:`);
+
+//aqui vou uconverte o nome para maiúsculas
+var nomeMaiusculo = nome.toUpperCase();
+
+//aqui vou converte o nome para minúsculas para ver como fica caso queira
+//var nomeMinusculo = nome.toLowerCase();
+//alert(`Seu nome em minúsculas: ${nomeMinusculo}`);
+
+//aqui vou xibe em um alerta
+alert(`Seu nome em maiúsculas: ${nomeMaiusculo}`);
 
