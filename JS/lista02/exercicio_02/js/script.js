f4b2fb540ceedd8211e5f67010a7939604ea8f44
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-11 23:24:51 -0300

    Update script.js

--- JS/lista02/exercicio_02/js/script.js
+++ JS/lista02/exercicio_02/js/script.js
@@ -1,1 +1,10 @@
 
+
+// Somamos 10 para que o intervalo comece em 10
+// ficaria sssim para arredondar.
+//numero = Math.floor(Math.random() * 41) + 10;
+
+numero = (Math.random() * 41) + 10;
+
+// document.write() exibe o resultado diretamente na página
+document.write("Número sorteado entre 10 e 50: " + numero);
