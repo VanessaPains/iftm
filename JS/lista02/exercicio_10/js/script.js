05b5587879d6fe4549bccd1025d136b43feb71ea
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-11 23:40:11 -0300

    Update script.js

--- JS/lista02/exercicio_10/js/script.js
+++ JS/lista02/exercicio_10/js/script.js
@@ -1,1 +1,13 @@
+// aqui o número total de cartas disponíveis
+const totalCartas = 27;
 
+// aqui vamos sorteia um número aleatório entre 1 e 27
+cartaSorteada = Math.floor(Math.random() * totalCartas) + 1;
+
+// aqui vamos define o caminho da imagem correspondente
+caminhoImagem = `img/carta${cartaSorteada}.png`;
+
+// aqui vou exibe a carta sorteada no corpo da página
+document.write("<h2>Carta Sorteada:</h2>");
+document.write(`<img src="${caminhoImagem}" alt="Carta ${cartaSorteada}" width="150">`);
+document.write(`<p>Esta é a carta número: ${cartaSorteada}</p>`);
