94aa88c43ce910581ff4a58f18067c317921c3fd
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-11 23:35:22 -0300

    Update script.js

--- JS/lista02/exercicio_06/js/script.js
+++ JS/lista02/exercicio_06/js/script.js
@@ -1,1 +1,12 @@
+// aqui vamos criar um array vazio para guardar os números
+numeros = [];
 
+// aqui fazemos um for para sortear 6 números aleatórios entre 1 e 60
+for (i = 0; i < 6; i++) {
+    numero = Math.floor(Math.random() * 60) + 1;
+    numeros.push(numero); //ele adiciona ao array
+}
+
+//aqui vamos exibe os números no corpo da página
+//o JOIN serve para colocar um separador entre os números
+document.write("Sugestão de números da Mega-Sena: " + numeros.join(" - "));
