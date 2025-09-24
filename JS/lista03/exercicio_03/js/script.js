5db9761b0d8c5f36f4f8457843554f471a6c799f
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-24 20:43:48 -0300

    Update script.js

--- JS/lista03/exercicio_03/js/script.js
+++ JS/lista03/exercicio_03/js/script.js
@@ -1,1 +1,9 @@
+btn = document.getElementById("btnDobro");
+txtValor = document.getElementById("txtValor");
 
+btn.addEventListener("click", exibirDobro);
+
+//para eu exibir o dobro do valor digitado no campo de texto
+function exibirDobro() {
+    txtResultado.value = (txtValor.value) * 2;
+}
