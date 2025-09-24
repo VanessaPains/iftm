5d570c957a05b7b38b988ae42e0a5967a2a232cd
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-24 20:46:36 -0300

    Update script.js

--- JS/lista03/exercicio_05/js/script.js
+++ JS/lista03/exercicio_05/js/script.js
@@ -1,1 +1,31 @@
+btn = document.getElementById("btnSoma");
+nota1 = parseFloat(document.getElementById("nota1").value);
+nota2 = parseFloat(document.getElementById("nota2").value);
+
+btn.addEventListener("click", exibirSoma);
+
+//para eu exibir a soma das notas e informar se o aluno foi aprovado ou reprovado
+function exibirSoma() {
+    //vo pegar os valores digitados
+    nota1 = Number(document.getElementById("nota1").value);
+    nota2 = Number(document.getElementById("nota2").value);
+
+    //aqui valido o intervalo 0 - 50 para cada bimestre
+    if (nota1 < 0 || nota1 > 50 || nota2 < 0 || nota2 > 50) {
+        alert("Cada nota bimestral deve estar entre 0 e 50 pontos.");
+        return;
+    }
+
+    //aqui soma as notas
+    soma = nota1 + nota2;
+
+    //vamos verifica se está aprovado
+    if (soma >= 60) {
+        alert("APROVADO! Soma: " + soma + " pontos.");
+    } else {
+        alert("REPROVADO. Faltaram: " + (60-soma) + " pontos.");
+    }
+}
+
+
 
