571c4e16c82735b88d10a837269419dfaa24a5e9
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-24 20:41:36 -0300

    Update script.js

--- JS/lista03/exercicio_01/js/script.js
+++ JS/lista03/exercicio_01/js/script.js
@@ -1,1 +1,11 @@
+btn = document.getElementById("btnExibir");
+txtNome = document.getElementById("txtNome");
+
+btn.addEventListener("click", exibirNome);
+
+//para exibir o nome digitado no campo de texto
+function exibirNome() {
+    alert(txtNome.value);
+}
+
 
