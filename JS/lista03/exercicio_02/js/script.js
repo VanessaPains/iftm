45c66b5e40e7520850c6f36a773564f3d4c90f4f
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-24 20:42:45 -0300

    Update script.js

--- JS/lista03/exercicio_02/js/script.js
+++ JS/lista03/exercicio_02/js/script.js
@@ -1,1 +1,11 @@
+btn = document.getElementById("btnExibir");
+txtNome = document.getElementById("txtNome");
+
+btn.addEventListener("click", exibirNome);
+
+//para exibir o nome digitado no campo de texto em letras maiúsculas
+function exibirNome() {
+    alert(txtNome.value.toUpperCase());
+}
+
 
