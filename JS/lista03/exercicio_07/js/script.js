80b3c7d15474d551d610a9641dc7246d4d061af9
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2025-09-24 20:49:48 -0300

    Update script.js

--- JS/lista03/exercicio_07/js/script.js
+++ JS/lista03/exercicio_07/js/script.js
@@ -1,1 +1,36 @@
+btnLimpar = document.getElementById("btnLimpar");
+btnEntrar = document.getElementById("btnEntrar");
 
+btnLimpar.addEventListener("click", limpar);
+btnEntrar.addEventListener("click", entrar);
+
+
+//para validar o formulario
+function entrar() {
+    login = document.getElementById("login").value.trim();
+    senha = document.getElementById("senha").value;
+    confirmaSenha = document.getElementById("confirmaSenha").value;
+
+    //1ª - Primeira validação: login não pode estar vazio
+    if (login === "") {
+        alert("O campo Login não pode estar vazio!");
+        return;
+    }
+
+    //2ª - Segunda validação: senhas iguais
+    if (senha !== confirmaSenha) {
+        alert("As senhas não conferem! Digite novamente.");
+        document.getElementById("senha").value = "";
+        document.getElementById("confirmaSenha").value = "";
+        return;
+    }
+
+    alert("Todos os campos foram digitados corretamente!");
+};
+
+//para limpar o formulario
+function limpar() {
+    document.getElementById("login").value = "";
+    document.getElementById("senha").value = "";
+    document.getElementById("confirmaSenha").value = "";
+};
