077b2cc28dd0d447d8d33884555dde7bf6e2e3c9
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2026-03-03 17:08:26 -0300

    Update userTables.js

--- PFESJ/lista_02/js/userTables.js
+++ PFESJ/lista_02/js/userTables.js
@@ -1,1 +1,15 @@
+export const userTable = [
+    {nome:"Ana", idade: 18, salario: 1000, sexo: "F"},
+    {nome:"Joao", idade: 20, salario: 3000, sexo: "M"},
+    {nome:"Cesar", idade: 33, salario: 1900, sexo: "M"},
+    {nome:"Maria", idade: 12, salario: 7000, sexo: "F"},
+    {nome:"Ze", idade: 17, salario: 2400, sexo: "M"},
+    {nome:"Vanessa", idade: 39, salario: 3800, sexo: "F"},
+    {nome:"Pedro", idade: 32, salario: 5000, sexo: "M"},
+    {nome:"Andressa", idade: 40, salario: 4800, sexo: "F"},
+    {nome:"Charles", idade: 38, salario: 7500, sexo: "M"},
+    {nome:"Alexa", idade: 17, salario: 1500, sexo: "F"},
+    {nome:"Andre", idade: 12, salario: 0, sexo: "M"}
+];
+
 
