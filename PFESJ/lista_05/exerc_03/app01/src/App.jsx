cf2fd4e6569afb6bed2526958dbeaae1451f4bb0
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2026-04-24 21:20:00 -0300

    Atividade Rect

---
+++ PFESJ/lista_05/exerc_03/app01/src/App.jsx
@@ -0,0 +1,19 @@
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ PFESJ/lista_05/exerc_03/app01/src/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card() {
+    return (
+        <div className="card">
+            <h2>Titulo do Card - Com React</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse unde nisi facilis magnam aliquid, dignissimos qui sapiente voluptatibus reiciendis perferendis id non cum sed quos suscipit corrupti ducimus accusantium exercitationem.</p>
+        </div>
+    );
+}
+
+export default Card;
